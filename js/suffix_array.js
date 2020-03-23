7415559fea98d5dca9ce1673b2f654aeedf817cb
OmarBurney <yenrubramo@gmail.com>
2020-03-23 03:07:57 -0700

    restructure Suffix Array & Display SAs on page

--- js/suffix_array.js
+++ js/suffix_array.js
@@ -21,12 +21,15 @@ class Three_Mer {
 }
 
 
-class Mer_lists {
-    constructor(kmers_12, sorted_kmers_12, kmers_0, sorted_kmers_0) {
+class MerLists {
+    constructor(kmers_12, kmers_0) {
         this.kmers_12 = kmers_12;
-        this.sorted_kmers_12 = sorted_kmers_12;
+        this.sorted_kmers_12 = kmers_12.slice();
         this.kmers_0 = kmers_0;
-        this.sorted_kmers_0 = sorted_kmers_0;
+        this.sorted_kmers_0 = kmers_0.slice();
+
+        this.sorted_kmers_12.sort(compare12Rank);
+        this.sorted_kmers_0.sort(compare0Rank);
     }
 }
 
@@ -38,6 +41,32 @@ function compareMer(a, b) {
 
     if (merA < merB) { comp = -1; }
     else if (merA > merB) { comp = 1; }
+
+    return comp;
+}
+
+
+function compare12Rank(a, b) {
+    const merA = a.rank;
+    const merB = b.rank;
+    let comp = 0;
+
+    if (merA < merB) { comp = -1; }
+    else if (merA > merB) { comp = 1; }
+
+    return comp;
+}
+
+
+function compare0Rank(a, b) {
+    const merA = a.rank;
+    const merB = b.rank;
+    let comp = 0;
+
+    if (merA.charAt(0) < merB.charAt(0)) { comp = -1; }
+    else if (merA.charAt(0) > merB.charAt(0)) { comp = 1; }
+    else if (parseInt(merA.substring(1)) < parseInt(merB.substring(1))) { comp = -1; }
+    else if (parseInt(merA.substring(1)) > parseInt(merB.substring(1))) { comp = 1; }
 
     return comp;
 }
@@ -62,54 +91,53 @@ function Unique_Ranks(skmer_12) {
 
 
 function Create_3_Mers(myString) {
-    let S = myString + "$$$";
-    let kmers_12 = [];
-    let kmers_0 = [];
-    let sorted_kmers_12 = [];
-    let sorted_kmers_0 = [];
-    let temp_mer = null;
+    let S = myString;
+    let m12 = [];
+    let m0 = [];
 
     // Add all 3-mers into the appropriate list
     for (let i = 0; i < S.length - 2; i++) {
         temp_mer = new Three_Mer(i, S.substring(i, i+3));
         
-        if ( i % 3 == 0 || i % 3 == 1 ) { kmers_12.push(temp_mer); }
-        else { kmers_0.push(temp_mer); }
+        if ( i % 3 == 0 || i % 3 == 1 ) { m12.push(temp_mer); }
+        else { m0.push(temp_mer); }
     }
 
-    sorted_kmers_12 = kmers_12.slice();
-    sorted_kmers_0 = kmers_0.slice();
-    sorted_kmers_12.sort(compareMer);
-
-    sorted_kmers_12[0].rank = "A";
-    for (let i = 1 ; i < sorted_kmers_12.length ; i++) {
-        if (sorted_kmers_12[i-1].chars == sorted_kmers_12[i].chars) {
-            sorted_kmers_12[i].rank = sorted_kmers_12[i-1].rank;
-        } else {
-            sorted_kmers_12[i].rank = nextCharacter(sorted_kmers_12[i-1].rank);
-        }
-    }
-    
-    // !!!!! Need different compare function ex "C7" to avoid equal 3mers
-    //       will need to sort this section after ranking
-    sorted_kmers_0.sort(compare);
-    
-    return new Mer_lists(kmers_12, sorted_kmers_12, kmers_0, sorted_kmers_0);
+    return {mer12: m12, mer0: m0};
 }
 
 
-function Merge_Mers(merlist_12, merlist_0) {
-    let mergedmer = [];
-    if (merlist_12.length == 0 && merlist_0.length == 0) { return mergedmer; }
-    if (merlist_0.length == 0) { return merlist_12; }
-    if (merlist_12.length == 0) { return merlist_0; }
+function RankMer12List(merlist) {
+    let sortedMers = merlist.slice();
+    sortedMers.sort(compareMer);
+    sortedMers[0].rank = "A";
+    for (let i = 1 ; i < sortedMers.length ; i++) {
+        if (sortedMers[i-1].chars == sortedMers[i].chars) {
+            sortedMers[i].rank = sortedMers[i-1].rank;
+        } else {
+            sortedMers[i].rank = nextCharacter(sortedMers[i-1].rank);
+        }
+    }
+}
 
-    let m12 = merlist_12.splice();
-    let m0 = merlist_0.splice();
+
+function RankMer0List(merlist, dict) {
+    for (let i = 0 ; i < merlist.length ; i++) {
+        merlist[i].rank = merlist[i].chars[0] + dict[parseInt(merlist[i].index) + 1];
+    }
+}
+
+
+function Merge_Mers(m12, m0, S12) {
+    let mergedmer = [];
     let iter12 = 0;
     let iter0 = 0;
 
-    while (iter12 < m12.length && iter0 < m12) {
+    if (m12.length == 0 && m0.length == 0) { return mergedmer; }
+    if (m0.length == 0) { return m12; }
+    if (m12.length == 0) { return m0; }
+
+    while (iter12 < m12.length && iter0 < m0.length) {
         if (m12[iter12].chars < m0[iter0].chars) {
             mergedmer.push(m12[iter12]);
             iter12++;
@@ -117,7 +145,33 @@ function Merge_Mers(merlist_12, merlist_0) {
             mergedmer.push(m0[iter0]);
             iter0++;
         } else {
-            if 
+            if (m12.index % 3 == 0) {
+                if (S12[iter12.index + 1] < S12[iter0 + 1]) {
+                    mergedmer.push(m12[iter12]);
+                    iter12++;
+                } else {
+                    mergedmer.push(m0[iter0]);
+                    iter0++;
+                }
+            } else { // m12.index % 3 == 1
+                if (S12[iter12.index + 2] < S12[iter0 + 2]) {
+                    mergedmer.push(m12[iter12]);
+                    iter12++;
+                } else {
+                    mergedmer.push(m0[iter0]);
+                    iter0++;
+                }
+            }
+        }
+    }
+
+    if (iter12 < m12.length) {
+        for (;iter12 < m12.length ; iter12++) {
+            mergedmer.push(m12[iter12]);
+        }
+    } else if (iter0 < m0.length) {
+        for (;iter0 < m0.length ; iter0++) {
+            mergedmer.push(m0[iter0]);
         }
     }
     
@@ -125,33 +179,76 @@ function Merge_Mers(merlist_12, merlist_0) {
 }
 
 
-function Suffix_Array(S) {
+function Suffix_Array(original_S) {
     let levels = [];
-    let KMers = Create_3_Mers(S);
-    levels.push({word: S, mers: KMers, SA: null})
-    
-    console.log(Unique_Ranks(KMers.sorted_kmers_12));
-    while (!Unique_Ranks(KMers.sorted_kmers_12)) {
-        let nS = "";
+    let KMers = null;
+    let S = original_S + "$$$";
+
+    do {
+        KMers = Create_3_Mers(S);
+        RankMer12List(KMers.mer12)
+        levels.push({word: S, mers: KMers, SA: {s12: null, s0: null, merged: null}});
+        S = "";
         // Append Mod 1s
-        for (let i = 0 ; i < KMers.kmers_12.length ; i = i + 2) { nS += KMers.kmers_12[i].rank; }
+        for (let i = 0 ; i < KMers.mer12.length ; i = i + 2) { S += KMers.mer12[i].rank; }
         // Append Mod 2s
-        for (let i = 1 ; i < KMers.kmers_12.length ; i = i + 2) { nS += KMers.kmers_12[i].rank; }
+        for (let i = 1 ; i < KMers.mer12.length ; i = i + 2) { S += KMers.mer12[i].rank; }
+        S += "$$$";
+    } while (!Unique_Ranks(KMers.mer12));
 
-        KMers = Create_3_Mers(nS);
-        levels.push({word: nS, mers: KMers})
-   }
+    let d = {}
+    let i = levels.length - 1;
 
-   let i = levels.length - 1;
-   //let tempSA = Merge_Mers(levels[i].KMers.sorted_kmers_12, levels[i].KMers.sorted_kmers_0);
-   //console.log(tempSA);
-   //for (let i = levels.length - 1 ; i >= 0 ; i--) {
-       
-   //}
+    let SA12 = levels[i].mers.mer12.slice();
+    let SA0 = levels[i].mers.mer0.slice();
+
+    SA12.sort(compare12Rank);
+    for (var j in SA12) {
+        d[SA12[j].index] = j;
+    }
+    levels[i].SA.s12 = SA12;
+
+    RankMer0List(KMers.mer0, d);
+    SA0.sort(compare0Rank);
+    levels[i].SA.s0 = SA0;
+
+    levels[i].SA.merged = Merge_Mers(levels[i].SA.s12, levels[i].SA.s0, d);
+    
+    i--;
+    let half = 0;
+    while (i >= 0) {
+        KMers = levels[i].mers;
+        SA0 = levels[i].mers.mer0.slice();
+
+        // convert SA of lower recursion to current level
+        SA12 = levels[i+1].SA.merged.slice();
+        half = Math.ceil((SA12.length - 1) / 2) - 1;
+        d = {};
+        for (let j = 0 ; j < SA12.length; j++) {
+            if (SA12[j].index <= half) {
+                d[3 * SA12[j].index] = j;
+            } else {
+                d[3 *(SA12[j].index - half - 1) + 1] = j;
+            }
+        }
+        
+        SA12 = new Array(levels[i].mers.mer12.length);
+        for (var j in levels[i].mers.mer12) {
+            SA12[d[levels[i].mers.mer12[j].index]] = levels[i].mers.mer12[j];
+        }
+        SA12.shift();
+        
+        levels[i].SA.s12 = SA12;
+        RankMer0List(KMers.mer0, d);
+        SA0.sort(compare0Rank);
+        levels[i].SA.s0 = SA0;
+
+        levels[i].SA.merged = Merge_Mers(levels[i].SA.s12, levels[i].SA.s0, d);
+        i--;
+    }
 
 
    return levels;
-
 }
 
 
@@ -228,19 +325,45 @@ function Create_String_Table(S) {
 }
 
 
-function Create_Step_Display(S, Mers) {
+function Create_SA_Table(SA, label) {
+    let container = null;
+    let table = null;
+    let row = null;
+    let cell = null;
+
+    container = document.createElement("DIV");
+
+    table = document.createElement("TABLE");
+    table.border = 1;
+
+    row = table.insertRow(-1);
+        cell = row.insertCell(-1);
+        cell.innerHTML = label + "=";
+        row.appendChild(cell);
+    for (var i in SA) {
+        cell = row.insertCell(-1);
+        cell.innerHTML = SA[i].index + 1;
+        row.appendChild(cell);
+    }
+    container.appendChild(table);
+
+    return container;
+}
+
+
+function Create_Step_Display(S, step) {
     let master_container = document.createElement("DIV");
     let row_container = null;
     let col_container = null;
-    
+    let Mers = new MerLists(step.mers.mer12, step.mers.mer0);
 
-    row_container = Create_String_Table(S + "$$$");
+    row_container = Create_String_Table(S);
     master_container.appendChild(row_container);
 
     // Display Mod 1,2
     row_container = document.createElement("DIV");
     row_container.className = "row";
-    col_container = Create_Mer_Table("MOD 1,2", Mers.kmers_12);
+    col_container = Create_Mer_Table("MOD 1,2", Mers.sorted_kmers_12);
     col_container.className = "col-sm";
     row_container.appendChild(col_container);
 
@@ -271,6 +394,28 @@ function Create_Step_Display(S, Mers) {
     row_container.appendChild(col_container);
     master_container.appendChild(row_container);
 
+    // Display SAs
+    row_container = document.createElement("DIV");
+    row_container.className = "row";
+    col_container = Create_SA_Table(step.SA.s12, "S12");
+    col_container.className = "col-sm"
+    row_container.appendChild(col_container);
+    master_container.appendChild(row_container);
+
+    row_container = document.createElement("DIV");
+    row_container.className = "row";
+    col_container = Create_SA_Table(step.SA.s0, "S0");
+    col_container.className = "col-sm"
+    row_container.appendChild(col_container);
+    master_container.appendChild(row_container);
+
+    row_container = document.createElement("DIV");
+    row_container.className = "row";
+    col_container = Create_SA_Table(step.SA.merged, "S");
+    col_container.className = "col-sm"
+    row_container.appendChild(col_container);
+    master_container.appendChild(row_container);
+
     return master_container;
 }
 
@@ -279,15 +424,18 @@ function Display_Algo() {
     let S = text_input.value;
     let container = null;
     let steps = Suffix_Array(S);
+    
+    console.log(steps);
 
     for (var i in steps) {
-        console.log(steps[i]);
+        //console.log(steps[i]);
 
         container = document.createElement("DIV");
         container.innerHTML = "STEP " + i;
+        container.id = "step-"+i;
         document.getElementById("step-by-step").appendChild(container);
 
-        container = Create_Step_Display(steps[i].word, steps[i].mers);
+        container = Create_Step_Display(steps[i].word, steps[i]);
         container.id = "step-wrapper";
         document.getElementById("step-by-step").appendChild(container);
     }
