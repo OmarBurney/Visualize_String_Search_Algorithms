7890ebdd5626249ef904341324778859580cd60c
OmarBurney <yenrubramo@gmail.com>
2020-03-28 15:06:44 -0700

    Test Include Vis.js Library

---
+++ js/suffix_tree.js
@@ -0,0 +1,36 @@
+import { DataSet } from "./node_modules/vis-data";
+import { Network } from "./node_modules/vis-network/peer/esm/vis-network";
+import "./node_modules/vis-network/styles/vis-network.css";
+
+
+// create an array with nodes
+const nodes = new DataSet([
+  { id: 1, label: "Node 1" },
+  { id: 2, label: "Node 2" },
+  { id: 3, label: "Node 3" },
+  { id: 4, label: "Node 4" },
+  { id: 5, label: "Node 5" }
+]);
+
+// create an array with edges
+const edges = new DataSet([
+  { from: 1, to: 3 },
+  { from: 1, to: 2 },
+  { from: 2, to: 4 },
+  { from: 2, to: 5 },
+  { from: 3, to: 3 }
+]);
+
+console.log(edges);
+console.log(nodes);
+
+// create a network
+const container = document.getElementById("mynetwork");
+const data = {
+  nodes: nodes,
+  edges: edges
+};
+
+const options = {};
+const network = new Network(container, data, options);
+
